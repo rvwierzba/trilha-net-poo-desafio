9195d623efc022b383bb1bc75fd03d464c5698d7
Rafael V. Wierzba <rafaelwierzba@gmail.com>
2024-08-14 14:18:24 -0300

    Resolvendo o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // TODO: Realizar os testes com as classes Nokia e Iphone
+        Iphone smt1 = new Iphone("8546565656", "Iphone 14", "8888888888888888888888888888", 1024);
+        smt1.Ligar();
+        smt1.ReceberLigacao();
+        smt1.InstalarAplicativo("AngryBird");
+
+        Nokia smt2 = new Nokia("9966969696696", "Nokia C03", "999999999999999999999999", 128);
+        smt2.Ligar();
+        smt2.ReceberLigacao();
+        smt2.InstalarAplicativo("X");
+    }
+}
